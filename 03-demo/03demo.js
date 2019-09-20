be5647c8f99e06be34ef49a00450d8d10e0bfce4
niuyuxin <1120733074@qq.com>
2019-09-20 10:33:03 +0800

    change

--- 03-demo/03demo.js
+++ 03-demo/03demo.js
@@ -7,8 +7,9 @@ var wd={
   'age':43
 };
 
-console.log("Name:%s\t Age:%d",wd.name,wd.age  );
-console.log('Wangding Info:',wd);
+console.log("Name:%s\t Age:%d",wd.name,wd.age);
+console.log('Wangding Info:',wd);//对象类型
 
-console.log('name:',wd.name);
-console.log('age is ${wd.age}');
+console.log('name:',wd.name);//逗号间隔输出
+console.log('QQ:'+wd.QQ);//拼接字符串输出
+console.log(`age is ${wd.age}`);//模板字符串输出
